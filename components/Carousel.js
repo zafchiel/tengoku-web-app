006d1de0c05c4246022f8d3173f897c29e7928b8
Zafchiel <bialakropla127.9@o2.pl>
2023-05-02 15:59:54 +0200

    Swiper is finally working correctly

--- components/Carousel.js
+++ components/Carousel.js
@@ -1,67 +1,64 @@
 import Image from "next/image"
-import { useRef, useEffect, useState } from "react"
-import { register } from "swiper/element/bundle"
+import { useEffect, useRef, useState } from "react"
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react"
+import { Autoplay } from "swiper"
 
-register()
+// Import Swiper styles
+import "swiper/css"
 
 export default function Carousel({ popularAnime, handleSlideChange }) {
-  const swiperElRef = useRef()
   const [loading, setLoading] = useState(true)
-  let arr = []
 
   useEffect(() => {
     setLoading(false)
   }, [])
 
-  useEffect(() => {
-    // listen for Swiper events using addEventListener
-    if (!loading) {
-      swiperElRef.current.addEventListener("slidechange", (e) => {
-        swiperElRef.activeIndex
-      })
-    }
-  }, [loading])
+  const handleChangeSlideIndex = (index) => {
+    handleSlideChange(index)
+  }
 
   return (
     <>
-      {!loading && (
-        <swiper-container
-          ref={swiperElRef}
-          slides-per-view="3"
-          loop="true"
-          autoplay="true"
-          autplay-delay="3000"
-        >
-          {popularAnime.map((obj, index) => (
-            <>
-              {arr.push(obj)}
-              <swiper-slide key={index}>
-                <div className="relative w-64 h-96 overflow-hidden">
-                  <Image
-                    src={obj.images.jpg.large_image_url}
-                    fill
-                    alt="image"
-                    className="rounded-lg"
-                    sizes="(max-width: 768px) 100vw,
+      (
+      <Swiper
+        slidesPerView={3}
+        loop
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        modules={[Autoplay]}
+        className="mySwiper"
+        onSlideChange={(e) => handleChangeSlideIndex(e.realIndex)}
+      >
+        {popularAnime.map((obj, index) => (
+          <>
+            <SwiperSlide key={index}>
+              <div className="relative w-64 h-96 overflow-hidden">
+                <Image
+                  src={obj.images.jpg.large_image_url}
+                  fill
+                  alt="image"
+                  className="rounded-lg"
+                  sizes="(max-width: 768px) 100vw,
               (max-width: 1200px) 50vw,
               33vw"
-                  />
-                  <div className="bg-black/50 absolute  bottom-0 left-0 p-3 w-full rounded-b-lg">
-                    <h1 className="text-xl text-white z-10 ">
-                      {obj.titles[0].title}
-                    </h1>
-                    <div className="flex gap-2 text-gray-300">
-                      {obj.genres.map((e, index) => (
-                        <p key={index}>{e.name}</p>
-                      ))}
-                    </div>
+                />
+                <div className="bg-black/50 absolute  bottom-0 left-0 p-3 w-full rounded-b-lg">
+                  <h1 className="text-xl text-white z-10 ">{obj.title}</h1>
+                  <div className="flex gap-2 text-gray-300">
+                    {obj.genres.map((e, index) => (
+                      <p key={index}>{e.name}</p>
+                    ))}
                   </div>
                 </div>
-              </swiper-slide>
-            </>
-          ))}
-        </swiper-container>
-      )}
+              </div>
+            </SwiperSlide>
+          </>
+        ))}
+      </Swiper>
+      )
     </>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import Image from "next/image"
 import Layout from "@/components/Layout"
 import Carousel from "@/components/Carousel"
 
@@ -11,13 +12,22 @@ export default function HomePage({ popularAnime }) {
     <>
       <Layout isTrasparent={true}>
         <main className="h-screen w-full flex items-center">
-          <div className="z-10 fixed top-0 left-0 bg-black/30 w-full h-screen"></div>
-          <iframe
+          <div className="z-10 fixed top-0 left-0 bg-black/30 w-full h-screen">
+            <Image
+              src={popularAnime[selectedHeaderIndex].images.jpg.large_image_url}
+              fill
+              alt="image"
+              className="object-cover w-full h-full"
+            />
+          </div>
+          {/* <iframe
             className="absolute w-full h-screen z-0"
             src={`${popularAnime[selectedHeaderIndex].trailer.embed_url}&mute=1&playsinline=1&controls=0&loop=1&playlist=${match[1]}`}
-          />
+          /> */}
           <section className="h-full w-1/4 flex flex-col justify-center items-center p-5 z-20 text-white">
-            <h1 className="text-6xl">TENGOKU</h1>
+            <h1 className="text-6xl">
+              {popularAnime[selectedHeaderIndex].title}
+            </h1>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua.
