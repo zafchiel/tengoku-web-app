e6a53499d2eaacb3c73b928d11a78ce263b820a4
Zafchiel <114311830+zafchiel@users.noreply.github.com>
2023-05-07 17:16:26 +0200

    Loading screen between pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,17 @@
 import { Inter } from "next/font/google"
+import { usePageLoading } from "@/hooks/usePageLoading"
 import "@/styles/globals.css"
+import LoadingPage from "@/components/LoadingPage"
 
 const inter = Inter({
   subsets: ["latin"],
 })
 
 export default function App({ Component, pageProps }) {
+  const { isPageLoading } = usePageLoading()
   return (
     <div className={inter.className}>
-      <Component {...pageProps} />
+      {isPageLoading ? <LoadingPage /> : <Component {...pageProps} />}
     </div>
   )
 }
