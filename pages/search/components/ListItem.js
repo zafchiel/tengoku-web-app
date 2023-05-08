26c1956a3e8ecb42ca714e9ade54011d02361901
Zafchiel <114311830+zafchiel@users.noreply.github.com>
2023-05-08 22:37:49 +0200

    image placeholder

--- pages/search/components/ListItem.js
+++ pages/search/components/ListItem.js
@@ -6,10 +6,15 @@ export default function ListItem({ data }) {
     <div className="flex flex-col overflow-hidden min-h-[200px] relative aspect-[2/3]">
       <Link href={`/details/${data?.mal_id}`}>
         <Image
-          width={100}
-          height={200}
+          width={500}
+          height={600}
           className="w-full hover:scale-110 duration-300 ease-in-out"
-          src={data?.images.jpg.image_url}
+          src={
+            data?.images.jpg.image_url !==
+            "https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png"
+              ? data?.images.jpg.image_url
+              : "/no_image.png"
+          }
           alt={data?.title}
         />
         <div className="bg-[#2a2c31] h-2/5 md:h-1/5 p-2 text-white z-20 absolute bottom-0 left-0 w-full">
